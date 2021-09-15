45c298a99c4c045bcef3e904b5361c8c255cbea4
PATIPHAN09 <76670584+PATIPHAN09@users.noreply.github.com>
2021-09-15 16:40:17 +0700

    D

--- main/loginprocess.php
+++ main/loginprocess.php
@@ -3,28 +3,19 @@ session_start();
 if(isset($_POST['save']))
 {
     extract($_POST);
-
+    $P = $_POST['password'];
     include 'database.php';
-    $sql = mysqli_query($conn,"SELECT * FROM userlogin");
-  
-
-
-
+    //$sql=mysqli_query($conn,"SELECT * FROM userlogin where password = '$P' and user = '$user'");
+    $sql=mysqli_query($conn,"SELECT * FROM userlogin where user = '$user' and  password = '$P'");
     $row  = mysqli_fetch_array($sql);
-    
-{
-    // your code here
-}
     if(is_array($row))
     {
-        $_SESSION["user"] = $row['user'];
-        $_SESSION["password"]=$row['password'];
-  
-        header("Location: home.php"); 
+      
+        header("Location: index.php"); 
     }
     else
     {
-        echo "Invalid Email ID/Password";
+        echo "Invalid Password = " . $P;
     }
 }
 ?>
--- main/signin.php
+++ main/signin.php
@@ -1,63 +1,33 @@
 <?php
-    session_start();
+session_start();
 ?>
 <!doctype html>
 <html class="fixed">
-	<head>
-        <?php include 'head.php';?>      
-	</head>
-	<body>
-		<!-- start: page -->
-		<section class="body-sign">
-			<div class="center-sign">
-				<div class="panel panel-sign">
-					<div class="panel-title-sign mt-xl text-right">
-						<h2 class="title text-uppercase text-bold m-none"><i class="fa fa-user mr-xs"></i> ล็อคอินเข้าสู่ระบบ</h2>
+<head>
+	<?php include 'head.php'; ?>
+</head>
+<body>
+	
+	<section class="body-sign">
+		<div class="center-sign">
+			<div class="panel-body">
+				<form action="loginprocess.php" method="post" enctype="multipart/form-data">
+					<h2>Login</h2>
+					<div class="form-group">
+						<input type="text" class="form-control" name="user" placeholder="user" required="required">
 					</div>
-					<form action="login.php" method="post" enctype="multipart/form-data">
-						<div class="panel-body">
-							<form action="index.html" method="post">
-								<div class="form-group mb-lg">
-									<label>Username</label>
-									<div class="input-group input-group-icon">
-										<input id="user" name="user" type="text" class="form-control input-lg" />
-										<span class="input-group-addon">
-											<span class="icon icon-lg">
-												<i class="fa fa-user"></i>
-											</span>
-										</span>
-									</div>
-								</div>
-
-								<div class="form-group mb-lg">
-									<div class="clearfix">
-										<label class="pull-left">Password</label>
-									</div>
-									<div class="input-group input-group-icon">
-										<input id="password" name="password" type="password" class="form-control input-lg" />
-										<span class="input-group-addon">
-											<span class="icon icon-lg">
-												<i class="fa fa-lock"></i>
-											</span>
-										</span>
-									</div>
-								</div>
-								
-								<div class="row">
-									<div class="col-sm-8">
-									</div>
-									<div class="col-sm-4 text-right">
-											<button type="submit" class="btn btn-primary hidden-xs">หลกตง</button>
-											<button type="submit" class="btn btn-primary btn-block btn-lg visible-xs mt-lg">หลกตง</button>
-									</div>
-								</div>
-							</form>
-						</div>
-				</div>
-				<p class="text-center text-muted mt-md mb-md">&copy; Copyright 2021. thank you very good  thank tood very mush <a href="https://google.com">Google</a>.</p>
+					<div class="form-group">
+						<input type="password" class="form-control"  name="password" placeholder="password" required="required">
+					</div>
+			
+					<div class="form-group">
+						<button type="submit" name="save" class="btn btn-success btn-lg btn-block">Login</button>
+					</div>
+				</form>
 			</div>
-		</section>
-		<!-- end: page -->
-        <?php include 'foot.php';?>      
-	</body>
+			<p class="text-center text-muted mt-md mb-md">&copy; Copyright 2021. thank you very good thank tood very mush <a href="https://google.com">Google</a>.</p>
+		</div>
+	</section>
+	<?php include 'foot.php'; ?>
+</body>
 </html>
